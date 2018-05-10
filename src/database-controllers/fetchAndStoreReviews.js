a4d20aaa82bb1c13b1cbda77d92a712c058e72f6
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-10 15:41:58 -0300

    add eslint

--- src/database-controllers/fetchAndStoreReviews.js
+++ src/database-controllers/fetchAndStoreReviews.js
@@ -3,6 +3,7 @@ const saveReviews = require('./saveReviews')
 const scrap = require('../scrap')
 
 const fetchAndStoreReviews = () => {
+  /* eslint-disable */
   console.log(new Date(), 'Fetching data from spreadsheet')
 
   return lastReview()
@@ -10,6 +11,7 @@ const fetchAndStoreReviews = () => {
     .then(saveReviews)
     .then(responses => console.log(new Date(), responses.length, 'new comments'))
     .catch(console.error)
+  /* eslint-enable */
 }
 
 module.exports = fetchAndStoreReviews
